dd3e0f755b0489db22b7d9c949a3d2d9f04d12f1
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-05-18 12:52:50 +0200

    Add redux store and services from the PoC

---
+++ src/core/store.ts
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query/react';
+import { userSliceReducer } from './services/user/user';
+import { fhirApi } from './services/fhir/fhir';
+
+export const store = configureStore({
+  reducer: {
+    [fhirApi.reducerPath]: fhirApi.reducer,
+    user: userSliceReducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(fhirApi.middleware),
+});
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
