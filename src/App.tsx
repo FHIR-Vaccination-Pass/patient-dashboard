ddd870a92f628f6346baf9169c8988ac800e2e92
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-05-18 13:14:23 +0200

    Reformat with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Or click <Link to="/403">{"here"}</Link> to get to nowhere!
+          Edit <code>src/App.tsx</code> and save to reload. Or click{' '}
+          <Link to='/403'>{'here'}</Link> to get to nowhere!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
-import {Button, ChakraProvider} from '@chakra-ui/react'
-import {theme} from "./theme/theme";
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import { Button, ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme/theme';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <ChakraProvider theme={theme}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/app" element={<App/>}/>
-                <Route
-                    path="/404"
-                    element={
-                        <main style={{padding: "1rem"}}>
-                            <p>404: ääätsch, there's nothing here! Go back with this <Link to="/app"><Button colorScheme='teal' variant='outline' size='xs'>Button</Button></Link></p>
-                        </main>
-                    }
-                />
-                <Route
-                    path="*"
-                    element={
-                        <Navigate to="/404" replace/>
-                    }
-                />
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/app' element={<App />} />
+        <Route
+          path='/404'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>
+                404: ääätsch, there's nothing here! Go back with this{' '}
+                <Link to='/app'>
+                  <Button colorScheme='teal' variant='outline' size='xs'>
+                    Button
+                  </Button>
+                </Link>
+              </p>
+            </main>
+          }
+        />
+        <Route path='*' element={<Navigate to='/404' replace />} />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
