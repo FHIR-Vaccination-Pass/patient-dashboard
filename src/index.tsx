32c61a752ea4eac6a35f50698cf57150ea565515
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-05-20 15:18:11 +0200

    Add proper not found component

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import SignIn from './views/Authentication/SignIn';
 import SignUp from './views/Authentication/SignUp';
 import ForgotPassword from './views/Authentication/ForgotPassword';
 import ResetPassword from './views/Authentication/ResetPassword';
+import NotFound from './views/NotFound';
 
 const root = document.getElementById('root') as HTMLElement;
 ReactDOM.render(
@@ -21,21 +22,7 @@ ReactDOM.render(
         <Route path='/signup' element={<SignUp />} />
         <Route path='/forgotpassword' element={<ForgotPassword />} />
         <Route path='/resetpassword' element={<ResetPassword />} />
-        <Route
-          path='/404'
-          element={
-            <main style={{ padding: '1rem' }}>
-              <p>
-                404: ääätsch, there's nothing here! Go back with this{' '}
-                <Link to='/'>
-                  <Button colorScheme='teal' variant='outline' size='xs'>
-                    Button
-                  </Button>
-                </Link>
-              </p>
-            </main>
-          }
-        />
+        <Route path='/404' element={<NotFound />} />
         <Route path='*' element={<Navigate to='/404' replace />} />
       </Routes>
     </BrowserRouter>
