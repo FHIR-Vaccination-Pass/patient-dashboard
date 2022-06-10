ec3a8d3acb8da9a0af6627fdaf2f13faba581edd
Dominik Fuchs <d0minik.fuchs@web.de>
2022-06-10 15:51:10 +0200

    add heading

--- src/views/Wiki/vaccineWiki.tsx
+++ src/views/Wiki/vaccineWiki.tsx
@@ -7,6 +7,7 @@ import {
   Box,
   Button,
   Divider,
+  Heading,
   HStack,
   IconButton,
 } from '@chakra-ui/react';
@@ -21,63 +22,66 @@ import { Link } from 'react-router-dom';
 export function VaccineWiki() {
   const vaccinations: Vaccination[] = getSomeVaccinations();
   return (
-    <Accordion defaultIndex={[]} allowMultiple>
-      {vaccinations.map((vaccination) => (
-        <>
-          <AccordionItem>
-            <h1>
-              <AccordionButton>
-                <Box fontSize='xl' flex='1' textAlign='left'>
-                  {vaccination.name}
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h1>
-            <AccordionPanel pb={4}>
-              <HStack style={{ position: 'relative' }}>
-                <div className={'vaccination-description'}>
-                  <span>
-                    <span className={'accordion-content-header'}>Disease:</span>{' '}
-                    {vaccination.name}
-                    <br />
-                  </span>
-                  <span>
-                    <span className={'accordion-content-header'}>
-                      Description:
-                    </span>{' '}
-                    {vaccination.description}
-                  </span>
-                  <Link to={`/dashboard/wiki/${vaccination.name}`}>
-                    <Button
-                      rightIcon={<ArrowForwardIcon />}
-                      variant='outline'
-                      colorScheme={'aviGreen'}
-                      mt={'20px'}
+      <Box style={{ marginLeft: '5px' }}>
+        <Heading>Wiki</Heading>
+        <Accordion defaultIndex={[]} allowMultiple>
+        {vaccinations.map((vaccination) => (
+            <div>
+              <AccordionItem>
+                <h1>
+                  <AccordionButton>
+                    <Box fontSize='xl' flex='1' textAlign='left'>
+                      {vaccination.name}
+                    </Box>
+                    <AccordionIcon/>
+                  </AccordionButton>
+                </h1>
+                <AccordionPanel pb={4}>
+                  <HStack style={{ position: 'relative' }}>
+                    <div className={'vaccination-description'}>
+                                  <span>
+                                      <span className={'accordion-content-header'}>Disease:</span>{' '}
+                                    {vaccination.name}
+                                    <br/>
+                                  </span>
+                      <span>
+                                      <span className={'accordion-content-header'}>
+                                          Description:
+                                      </span>{' '}
+                        {vaccination.description}
+                                  </span>
+                      <Link to={`/dashboard/wiki/${vaccination.name}`}>
+                        <Button
+                            rightIcon={<ArrowForwardIcon/>}
+                            variant='outline'
+                            colorScheme={'aviGreen'}
+                            mt={'20px'}
+                        >
+                          Learn more
+                        </Button>
+                      </Link>
+                    </div>
+                    <HStack
+                        style={{height: '70px', position: 'absolute', right: 0}}
                     >
-                      Learn more
-                    </Button>
-                  </Link>
-                </div>
-                <HStack
-                  style={{ height: '70px', position: 'absolute', right: 0 }}
-                >
-                  <Divider orientation={'vertical'} />
-                  <Link to={`/dashboard/wiki/${vaccination.name}`}>
-                    <IconButton
-                      className={'extend-full-height'}
-                      aria-label={'Get details'}
-                      size={'lg'}
-                      colorScheme='Gray'
-                      variant='ghost'
-                      icon={<ChevronRightIcon />}
-                    ></IconButton>
-                  </Link>
-                </HStack>
-              </HStack>
-            </AccordionPanel>
-          </AccordionItem>
-        </>
-      ))}
-    </Accordion>
+                      <Divider orientation={'vertical'}/>
+                      <Link to={`/dashboard/wiki/${vaccination.name}`}>
+                        <IconButton
+                            className={'extend-full-height'}
+                            aria-label={'Get details'}
+                            size={'lg'}
+                            colorScheme='Gray'
+                            variant='ghost'
+                            icon={<ChevronRightIcon/>}
+                        ></IconButton>
+                      </Link>
+                    </HStack>
+                  </HStack>
+                </AccordionPanel>
+              </AccordionItem>
+            </div>
+        ))}
+      </Accordion>
+</Box>
   );
 }
