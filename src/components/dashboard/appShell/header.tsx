c9a6c1ade85d647b88540c72cab170f98a7ca5ea
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-06-10 12:14:00 +0200

    Switch postion of user and logo. Fix alignment issues in header

--- src/components/dashboard/appShell/header.tsx
+++ src/components/dashboard/appShell/header.tsx
@@ -12,9 +12,9 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import { MobileMenu } from './mobileMenu';
-import VaccinationPass from '../../../assets/VaccinationPass.png';
 import VaccinationPassIcon from '../../../assets/VaccinationPassIcon.png';
 import { Link } from 'react-router-dom';
+import VaccinationPass from '../../../assets/VaccinationPass.png';
 
 interface DashboardProps extends BoxProps {
   onOpen: () => void;
@@ -37,47 +37,14 @@ export const DashboardHeader = ({
       w={'100%'}
       {...rest}
     >
-      <Box
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.15)'
-        borderRadius={'20px'}
-        w={'200px'}
-        mt={'10px'}
-        ml={'10px'}
-        display={{ base: 'flex', md: 'none' }}
-      >
-        <Flex p='5%' flexDir='column' w='100%' alignItems={'flex-start'}>
-          <Flex align='center' onClick={onOpen}>
-            <Avatar size='md' src='avatar-1.jpg' />
-            <Flex flexDir='column' ml={4} display={'flex'}>
-              <Heading as='h3' size='sm'>
-                Sylwia Weller
-              </Heading>
-              <Text color='gray'>Admin</Text>
-            </Flex>
-          </Flex>
-        </Flex>
-
-        <Modal
-          isOpen={isOpen}
-          onClose={onClose}
-          size={'full'}
-          isCentered={true}
-        >
-          <MobileMenu isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
-            {' '}
-          </MobileMenu>
-        </Modal>
-      </Box>
       <Flex
         bg={useColorModeValue('white', 'gray.900')}
         boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.15)'
         borderRadius={'20px'}
-        w={'80%'}
+        w={'100%'}
         mt={'10px'}
         mr={'10px'}
         p={'10px'}
-        flexBasis={'stretch'}
         alignItems={'center'}
         display={{ base: 'none', md: 'flex' }}
       >
@@ -93,30 +60,68 @@ export const DashboardHeader = ({
         bg={useColorModeValue('white', 'gray.900')}
         boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.15)'
         borderRadius={'20px'}
-        w={'250px'}
         mt={'10px'}
-        mr={'10px'}
+        ml={'10px'}
         p={'10px'}
         alignItems={'center'}
+        display={{ base: 'block', md: 'none' }}
         _hover={{ boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.30)' }}
       >
         <Link to={'/dashboard'}>
-          <Image
-            src={VaccinationPass}
-            w={'200px'}
-            h={'40px'}
-            align={'center'}
-            display={{ base: 'none', md: 'block' }}
-          />
-          <Image
-            src={VaccinationPassIcon}
-            w={'40px'}
-            h={'40px'}
-            align={'center'}
-            display={{ base: 'block', md: 'none' }}
-          />
+          <Image src={VaccinationPass} w={'200px'} align={'center'} />
         </Link>
       </Flex>
+
+      {/* Mobile */}
+      <Box
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.15)'
+        borderRadius={'20px'}
+        w={'60px'}
+        mt={'10px'}
+        mr={'10px'}
+        display={{ base: 'flex', md: 'none' }}
+      >
+        <Flex m='auto' flexDir='column' w='100%' alignItems={'center'}>
+          <Flex align='center' onClick={onOpen}>
+            <Avatar size='md' src='avatar-1.jpg' />
+          </Flex>
+        </Flex>
+
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          size={'full'}
+          isCentered={true}
+        >
+          <MobileMenu isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
+            {' '}
+          </MobileMenu>
+        </Modal>
+      </Box>
+
+      {/* Desktop */}
+      <Box
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.15)'
+        borderRadius={'20px'}
+        w={'350px'}
+        mt={'10px'}
+        mr={'10px'}
+        display={{ base: 'none', md: 'flex' }}
+      >
+        <Flex p='5%' flexDir='column' w='100%' alignItems={'flex-start'}>
+          <Flex align='center'>
+            <Avatar size='md' src='avatar-1.jpg' />
+            <Flex flexDir='column' ml={4} display={'flex'}>
+              <Heading as='h3' size='sm'>
+                Sylwia Weller
+              </Heading>
+              <Text color='gray'>Admin</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
     </Flex>
   );
 };
