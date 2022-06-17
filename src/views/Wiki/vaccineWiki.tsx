f9e46cead6f53b46f7098935bd8f07ab2aebd01c
Dominik Fuchs <d0minik.fuchs@web.de>
2022-06-17 10:41:26 +0200

    Make wiki suitable for mobile

--- src/views/Wiki/vaccineWiki.tsx
+++ src/views/Wiki/vaccineWiki.tsx
@@ -1,87 +1,39 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  Button,
-  Divider,
-  Heading,
-  HStack,
-  IconButton,
-} from '@chakra-ui/react';
+import { Box, Divider, Heading, HStack, IconButton } from '@chakra-ui/react';
 import {
   getSomeVaccinations,
   Vaccination,
 } from '../../assets/models/vaccination';
 import './vaccineWiki.css';
-import { ArrowForwardIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 
 export function VaccineWiki() {
   const vaccinations: Vaccination[] = getSomeVaccinations();
   return (
-      <Box style={{ marginLeft: '5px' }}>
-        <Heading>Wiki</Heading>
-        <Accordion defaultIndex={[]} allowMultiple>
-        {vaccinations.map((vaccination) => (
-            <div>
-              <AccordionItem>
-                <h1>
-                  <AccordionButton>
-                    <Box fontSize='xl' flex='1' textAlign='left'>
-                      {vaccination.name}
-                    </Box>
-                    <AccordionIcon/>
-                  </AccordionButton>
-                </h1>
-                <AccordionPanel pb={4}>
-                  <HStack style={{ position: 'relative' }}>
-                    <div className={'vaccination-description'}>
-                                  <span>
-                                      <span className={'accordion-content-header'}>Disease:</span>{' '}
-                                    {vaccination.name}
-                                    <br/>
-                                  </span>
-                      <span>
-                                      <span className={'accordion-content-header'}>
-                                          Description:
-                                      </span>{' '}
-                        {vaccination.description}
-                                  </span>
-                      <Link to={`/dashboard/wiki/${vaccination.name}`}>
-                        <Button
-                            rightIcon={<ArrowForwardIcon/>}
-                            variant='outline'
-                            colorScheme={'aviGreen'}
-                            mt={'20px'}
-                        >
-                          Learn more
-                        </Button>
-                      </Link>
-                    </div>
-                    <HStack
-                        style={{height: '70px', position: 'absolute', right: 0}}
-                    >
-                      <Divider orientation={'vertical'}/>
-                      <Link to={`/dashboard/wiki/${vaccination.name}`}>
-                        <IconButton
-                            className={'extend-full-height'}
-                            aria-label={'Get details'}
-                            size={'lg'}
-                            colorScheme='Gray'
-                            variant='ghost'
-                            icon={<ChevronRightIcon/>}
-                        ></IconButton>
-                      </Link>
-                    </HStack>
-                  </HStack>
-                </AccordionPanel>
-              </AccordionItem>
-            </div>
-        ))}
-      </Accordion>
-</Box>
+    <Box style={{ marginLeft: '5px' }}>
+      <Heading mb={'20px'}>Wiki</Heading>
+      {vaccinations.map((vaccination) => (
+        <div>
+          <Divider orientation='horizontal' mb={'10px'} />
+          <Link to={`/dashboard/wiki/${vaccination.name}`}>
+            <HStack style={{ position: 'relative' }}>
+              <Heading as='h3' size='lg'>
+                {vaccination.name}
+              </Heading>
+              <IconButton
+                className={'extend-full-height'}
+                aria-label={'Get details'}
+                size={'lg'}
+                colorScheme='Gray'
+                variant='ghost'
+                icon={<ChevronRightIcon />}
+                style={{ position: 'absolute', right: '0', top: '-7px' }}
+              ></IconButton>
+            </HStack>
+          </Link>
+          <Divider orientation='horizontal' mt={'10px'} />
+        </div>
+      ))}
+    </Box>
   );
 }
