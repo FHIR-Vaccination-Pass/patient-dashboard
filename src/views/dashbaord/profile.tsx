4be595148f7de69bf166fec2e45d40f9ba2e8097
Dominik Fuchs <d0minik.fuchs@web.de>
2022-06-24 14:57:37 +0200

    Lint profile.tsx

--- src/views/dashbaord/profile.tsx
+++ src/views/dashbaord/profile.tsx
@@ -1,11 +1,17 @@
-import {Box, Button, Text} from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 import { FaSignOutAlt } from 'react-icons/fa';
 
 export function Profile() {
   return (
     <Box bg={'gray.100'} h={'100%'} position={'relative'}>
       <Text> Hey Profile! </Text>
-      <Button style={{position: 'absolute', bottom: 0, left: 0}} leftIcon={<FaSignOutAlt />} colorScheme='teal' variant='solid' size={'md'}>
+      <Button
+        style={{ position: 'absolute', bottom: 0, left: 0 }}
+        leftIcon={<FaSignOutAlt />}
+        colorScheme='teal'
+        variant='solid'
+        size={'md'}
+      >
         Logout
       </Button>
     </Box>
