63ae5638dfb6affb895571cb97c98c103cadfaf8
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-06-30 10:31:34 +0200

    Remove desktop layout from dashboard overview component

--- src/views/dashbaord/overview.tsx
+++ src/views/dashbaord/overview.tsx
@@ -1,5 +1,4 @@
 import { Grid, GridItem } from '@chakra-ui/react';
-import { VaccineHistoryWidget } from '../../components/dashboard/vaccineHistoryWidget/vaccineHistoryWidget';
 
 export function Overview() {
   return (
@@ -10,43 +9,12 @@ export function Overview() {
       templateColumns='repeat(12, 1fr)'
       gap={5}
     >
-      {/* Desktop */}
-      <GridItem
-        display={{ base: 'none', md: 'grid' }}
-        colSpan={8}
-        rowSpan={6}
-        bg={'gray.300'}
-      ></GridItem>
-
-      <GridItem display={{ base: 'none', md: 'grid' }} rowSpan={12} colSpan={4}>
-        <VaccineHistoryWidget />
-      </GridItem>
-
-      <GridItem
-        display={{ base: 'none', md: 'grid' }}
-        colSpan={8}
-        rowSpan={6}
-        bg={'gray.300'}
-      ></GridItem>
-
       {/* Mobile */}
-      <GridItem display={{ base: 'grid', md: 'none' }} colSpan={12} rowSpan={4}>
-        <VaccineHistoryWidget />
-      </GridItem>
+      <GridItem colSpan={12} rowSpan={4} bg={'gray.300'}></GridItem>
 
-      <GridItem
-        display={{ base: 'grid', md: 'none' }}
-        colSpan={12}
-        rowSpan={4}
-        bg={'gray.300'}
-      ></GridItem>
+      <GridItem colSpan={12} rowSpan={4} bg={'gray.300'}></GridItem>
 
-      <GridItem
-        display={{ base: 'grid', md: 'none' }}
-        colSpan={12}
-        rowSpan={4}
-        bg={'gray.300'}
-      ></GridItem>
+      <GridItem colSpan={12} rowSpan={4} bg={'gray.300'}></GridItem>
     </Grid>
   );
 }
