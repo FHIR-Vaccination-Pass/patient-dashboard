d1120d1f6230b2fc62037c3fb420d3ae6557debe
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-01 16:06:58 +0200

    Add fhir support to status cards and make micro interactions dependent on recommendation status field.

--- src/components/dashboard/immunizationStatus/immunizationStatusCard.tsx
+++ src/components/dashboard/immunizationStatus/immunizationStatusCard.tsx
@@ -1,15 +1,23 @@
 import { Flex, Icon, Stack, Text } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { AggregatedImmunizationStatus } from '../../../core/models/aggregatedImmunizationStatus';
+import {
+  CompleteStatus,
+  DueStatus,
+  MissingHistoryStatus,
+  OverdueStatus,
+} from './immunizationCardConfigurations';
+import { ImmunizationRecommendationRecommendation } from 'fhir/r4';
 
 interface ImmunizationStatusCardProps {
-  status: AggregatedImmunizationStatus;
+  recommendations: ImmunizationRecommendationRecommendation[];
 }
 
 export const ImmunizationStatusCard: FC<ImmunizationStatusCardProps> = ({
-  status,
+  recommendations,
 }) => {
-  let currentStatus: string = 'default';
+  let status: AggregatedImmunizationStatus =
+    calcAggregateImmunizationStatus(recommendations);
   return (
     <Flex
       bg={status.backgroundColor}
@@ -35,3 +43,40 @@ export const ImmunizationStatusCard: FC<ImmunizationStatusCardProps> = ({
     </Flex>
   );
 };
+
+function calcAggregateImmunizationStatus(
+  recommendations: ImmunizationRecommendationRecommendation[]
+): AggregatedImmunizationStatus {
+  // If no recommendations or no recomendation status fields set, then immunization complete
+  let aggregatedImmunizationStatus = CompleteStatus;
+  // Do later: Check history and recommendations to set missing history status
+  let currentStatus: string | undefined = '';
+  recommendations.forEach(
+    (recommendation: ImmunizationRecommendationRecommendation) => {
+      currentStatus = recommendation.forecastStatus.text;
+      // When current status is undefined then don't change anything.
+      console.log('currentStatus: ' + currentStatus);
+      if (currentStatus !== undefined) {
+        // Due > Complete & MissingHistory
+        if (
+          currentStatus === 'due' &&
+          (aggregatedImmunizationStatus === CompleteStatus ||
+            aggregatedImmunizationStatus === MissingHistoryStatus)
+        ) {
+          aggregatedImmunizationStatus = DueStatus;
+          // Overdue > all else
+        } else if (currentStatus === 'overdue') {
+          aggregatedImmunizationStatus = OverdueStatus;
+          // Complete = complete or immune or contraindicated
+        } else if (
+          aggregatedImmunizationStatus !== OverdueStatus &&
+          aggregatedImmunizationStatus !== DueStatus
+        ) {
+          aggregatedImmunizationStatus = CompleteStatus;
+        }
+      }
+    }
+  );
+  console.log(aggregatedImmunizationStatus);
+  return aggregatedImmunizationStatus;
+}
--- src/components/dashboard/immunizationStatus/recommendationCard.tsx
+++ src/components/dashboard/immunizationStatus/recommendationCard.tsx
@@ -8,55 +8,65 @@ import {
   useToken,
 } from '@chakra-ui/react';
 import React, { FC } from 'react';
-import { FaViruses } from 'react-icons/fa';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
-import { ImmunizationRecommendation } from '../../../core/models/immunizationRecommendation';
+import { getColorByStatus, getIconByStatus } from '../../../theme/theme';
+import { ImmunizationRecommendationRecommendation } from 'fhir/r4';
 
 interface RecommendationCardProps extends BoxProps {
-  status: ImmunizationRecommendation;
+  configuration: ImmunizationRecommendationRecommendation;
 }
 
-export const RecommendationCard: FC<RecommendationCardProps> = ({ status }) => {
+export const RecommendationCard: FC<RecommendationCardProps> = ({
+  configuration,
+}) => {
   const [color] = useToken(
     // the key within the theme, in this case `theme.colors`
     'colors',
     // the subkey(s), resolving to `theme.colors.red.100`
-    [status.backgroundColor]
+    [getColorByStatus(configuration.forecastStatus.text) + '.300']
     // a single fallback or fallback array matching the length of the previous arg
   );
   return (
-    <Link to={`/dashboard/wiki/${status.code}`}>
+    <Link to={`/dashboard/wiki/${configuration.targetDisease?.text}`}>
       <Flex
         justifyContent={'space-between'}
         alignItems={'center'}
         borderRadius={'15px'}
         border={'1px solid'}
-        borderColor={status.backgroundColor}
-        boxShadow={`0 0 5px 0.5px ${color}`}
+        borderColor={color}
+        boxShadow={`0 0 2px 0.5px ${color}`}
       >
-        <Flex>
-          <Icon
-            mt={'auto'}
-            mb={'auto'}
-            ml='3'
-            as={FaViruses}
-            color={'brand.400'}
-            fontSize={'30pt'}
-          />
+        <Flex justifyContent={'space-between'} w={'90%'}>
           <Box p='3'>
-            <Text fontWeight='bold'>{status.diseaseName}</Text>
+            <Text fontWeight='bold'>{configuration.targetDisease?.text}</Text>
             <Text>
               Due:
               <Badge
                 fontSize={'sm'}
-                colorScheme={status.backgroundColor}
+                colorScheme={getColorByStatus(
+                  configuration.forecastStatus.text
+                )}
                 ml={5}
               >
-                {status.due.toDateString()}
+                {configuration.dateCriterion !== undefined &&
+                  configuration.dateCriterion[0].value}
               </Badge>
             </Text>
           </Box>
+          {getIconByStatus(configuration.forecastStatus.text) !== undefined && (
+            <Icon
+              mt={'auto'}
+              mb={'auto'}
+              ml='3'
+              as={getIconByStatus(configuration.forecastStatus.text)}
+              color={
+                getColorByStatus(configuration.forecastStatus.text) + '.400'
+              }
+              w={6}
+              h={6}
+            />
+          )}
         </Flex>
         <ChevronRightIcon w={8} h={8} m={4} />
       </Flex>
