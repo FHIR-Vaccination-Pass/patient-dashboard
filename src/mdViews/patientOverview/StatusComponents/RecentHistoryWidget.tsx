f873d45464af315ed953941c1fbfbdff061ec66a
Nils Krüger <nils.krueger@tum.de>
2022-08-20 23:00:45 +0200

    feat: #132 code style fixes

--- src/mdViews/patientOverview/StatusComponents/RecentHistoryWidget.tsx
+++ src/mdViews/patientOverview/StatusComponents/RecentHistoryWidget.tsx
@@ -1,53 +1,35 @@
 import React, { FC } from 'react';
-import {
-  Badge,
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Stack,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Badge, Box, Divider, Flex, Stack, Text } from '@chakra-ui/react';
 import { Link, useParams } from 'react-router-dom';
 import {
-  useImmunizationRecommendations,
   useImmunizations,
   useMedicationInfo,
   useMedications,
-  usePractitioners,
-  useTargetDiseases,
 } from '../../../hooks';
-import { ImmunizationRecommendation } from '../../../core/models';
 import { skipToken } from '@reduxjs/toolkit/query';
 
-export const RecentHistoryWidget: FC = ({}) => {
+export const RecentHistoryWidget: FC = () => {
   const params = useParams();
   const patientId = params['patientId'];
-  const { data: medicationsData, idToMedication } = useMedications({});
 
-  const { data: immunizations, idToImmunization } = useImmunizations(
-    medicationsData
+  const { immunizations } = useImmunizations({
+    patient: patientId,
+  });
+  const {
+    data: medicationsData,
+    medications,
+    idToMedication,
+  } = useMedications(
+    immunizations
       ? {
-          'vaccine-code': medicationsData.ids
-            .map(idToMedication)
-            .map((m) => m!.code.coding.code)
+          code: immunizations
+            .map((imm) => imm.vaccineCode.coding.code)
             .join(','),
         }
       : skipToken
   );
-  const { idToTargetDisease } = useTargetDiseases({});
-
-  const {
-    vaccinationSchemes,
-    idToVaccinationScheme,
-    vaccinationDoses,
-    idToVaccinationDose,
-  } = useMedicationInfo(
-    medicationsData?.ids.map((id: string) => idToMedication(id)!)
-  );
-
-  const standardVaccinationSchemes = vaccinationSchemes?.byType['standard'];
+  const { vaccinationDoses, idToVaccinationDose } =
+    useMedicationInfo(medications);
 
   return (
     <Flex
@@ -60,84 +42,70 @@ export const RecentHistoryWidget: FC = ({}) => {
       w={'100%'}
     >
       <Box overflowY={'scroll'}>
-        {immunizations ? (
-          immunizations.ids
-            .slice()
-            .sort(
-              (a, b) =>
-                (idToImmunization(b)?.occurrenceTime?.getTime()?.valueOf() ??
-                  0) -
-                (idToImmunization(a)?.occurrenceTime?.getTime()?.valueOf() ?? 0)
-            )
-            .map((iId: string) => {
-              const imm = idToImmunization(iId);
-              const med =
-                imm &&
-                idToMedication(
-                  medicationsData?.byCode[imm.vaccineCode.coding.code]?.ids[0]
-                );
-              const vs =
-                med &&
-                idToVaccinationScheme(
-                  standardVaccinationSchemes?.byMedication[med.id]?.ids[0]
-                );
-              const allDoses =
-                vs &&
-                vaccinationDoses?.byVaccinationScheme[vs.id]?.ids.map(
-                  idToVaccinationDose
-                );
-              const dose = idToVaccinationDose(imm?.vaccinationDoseId);
-              return (
-                <Stack>
-                  <Flex
-                    justifyContent={'space-between'}
-                    w={'100%'}
-                    alignItems={'center'}
-                    p={4}
-                    pl={6}
-                    pr={6}
-                  >
-                    <Text w={'10vw'}> {med?.tradeName} </Text>
-                    <Divider orientation={'vertical'} h={'40px'} />
-                    <Flex
-                      w={'50%'}
-                      justifyContent={'end'}
-                      alignItems={'center'}
-                    >
-                      {med && vs && allDoses && dose && (
-                        <>
-                          <Badge
-                            colorScheme={'orange'}
-                            variant='subtle'
-                            w={'200px'}
-                            textAlign={'center'}
-                            mr={3}
-                          >
-                            Dose:{' '}
-                            {'numberInScheme' in dose!
-                              ? `${dose.numberInScheme} / ${allDoses!.length}`
-                              : 'Booster'}
-                          </Badge>
-                          <Badge
-                            colorScheme={'blue'}
-                            variant='subtle'
-                            w={'100px'}
-                            textAlign={'center'}
-                            mr={3}
-                          >
-                            {imm?.occurrenceTime.toLocaleDateString()}
-                          </Badge>
-                        </>
-                      )}
-                    </Flex>
-                  </Flex>
-                  <Divider />
-                </Stack>
+        {immunizations
+          ?.slice()
+          .sort(
+            (a, b) =>
+              (b.occurrenceTime?.getTime()?.valueOf() ?? 0) -
+              (a.occurrenceTime?.getTime()?.valueOf() ?? 0)
+          )
+          .map((imm) => {
+            const med =
+              imm &&
+              idToMedication(
+                medicationsData?.byCode[imm.vaccineCode.coding.code]?.ids[0]
               );
-            })
-        ) : (
-          <> </>
-        )}
+            const dose = idToVaccinationDose(imm?.vaccinationDoseId);
+            const allDoses =
+              dose &&
+              vaccinationDoses?.byVaccinationScheme[
+                dose.vaccinationSchemeId
+              ]?.ids.map(idToVaccinationDose);
+
+            return (
+              <Stack>
+                <Flex
+                  justifyContent={'space-between'}
+                  w={'100%'}
+                  alignItems={'center'}
+                  p={4}
+                  pl={6}
+                  pr={6}
+                >
+                  <Text w={'10vw'}> {med?.tradeName} </Text>
+                  <Divider orientation={'vertical'} h={'40px'} />
+                  <Flex w={'50%'} justifyContent={'end'} alignItems={'center'}>
+                    {med && allDoses && dose && (
+                      <>
+                        <Badge
+                          colorScheme={'orange'}
+                          variant='subtle'
+                          w={'200px'}
+                          textAlign={'center'}
+                          mr={3}
+                        >
+                          Dose:{' '}
+                          {'numberInScheme' in dose!
+                            ? `${dose.numberInScheme} / ${allDoses!.length}`
+                            : 'Booster'}
+                        </Badge>
+                        <Badge
+                          colorScheme={'blue'}
+                          variant='subtle'
+                          w={'100px'}
+                          textAlign={'center'}
+                          mr={3}
+                        >
+                          {imm?.occurrenceTime.toLocaleDateString()}
+                        </Badge>
+                      </>
+                    )}
+                  </Flex>
+                </Flex>
+                <Divider />
+              </Stack>
+            );
+          })}
       </Box>
       <Box>
         <Divider orientation='horizontal' />
