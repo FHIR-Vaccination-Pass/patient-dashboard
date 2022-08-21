18ec4abb431de1f1854e07e0913aea29543f12c8
Nils Krüger <nils.krueger@tum.de>
2022-08-21 19:55:02 +0200

    feat: #129 add mutation methods to ImmunizationRecommendationMapper

--- src/core/models/ImmunizationRecommendation.ts
+++ src/core/models/ImmunizationRecommendation.ts
@@ -4,12 +4,12 @@ import {
   Extension as FHIRExtension,
   ImmunizationRecommendation as FHIRImmunizationRecommendation,
   ImmunizationRecommendationRecommendationDateCriterion as FHIRImmunizationRecommendationRecommendationDateCriterion,
-  Reference as FHIRReference,
 } from 'fhir/r4';
 import fhirpath from 'fhirpath';
 import fhirpath_r4_model from 'fhirpath/fhir-context/r4';
 import { settings } from '../../settings';
 import dayjs from 'dayjs';
+import { cloneDeep } from 'lodash';
 
 export type ImmunizationRecommendationStatus =
   | 'complete'
@@ -64,6 +64,90 @@ export class ImmunizationRecommendationMapper
       this.fromResource(id === undefined ? undefined : lookupFunc(id));
   }
 
+  static fromModel({
+    date,
+    forecastStatus,
+    vaccineCode,
+    targetDisease,
+    recommendedStartDate,
+    isDeactivated,
+    supportingImmunizationIds,
+    fulfillingImmunizationIds,
+    patientId,
+    populationRecommendationId,
+    vaccinationDoseId,
+  }: ImmunizationRecommendation): ImmunizationRecommendationMapper {
+    return new ImmunizationRecommendationMapper({
+      resourceType: 'ImmunizationRecommendation',
+      meta: {
+        profile: [
+          `${settings.fhir.profileBaseUrl}/vp-immunization-recommendation`,
+        ],
+      },
+      date: dayjs(date).format('YYYY-MM-DD'),
+      patient: { reference: `Patient/${patientId}` },
+      recommendation: [
+        {
+          forecastStatus: {
+            coding: [
+              {
+                system: forecastStatus.coding.system,
+                code: forecastStatus.coding.code,
+              },
+            ],
+          },
+          vaccineCode: [
+            {
+              coding: [
+                {
+                  system: vaccineCode.coding.system,
+                  code: vaccineCode.coding.code,
+                },
+              ],
+            },
+          ],
+          targetDisease: {
+            coding: [
+              {
+                system: targetDisease.coding.system,
+                code: targetDisease.coding.code,
+              },
+            ],
+          },
+          dateCriterion: [
+            {
+              code: {
+                coding: [{ system: 'http://loinc.org', code: '30981-5' }],
+              },
+              value: dayjs(recommendedStartDate).format('YYYY-MM-DD'),
+            },
+          ],
+          supportingImmunization: supportingImmunizationIds.map((siId) => ({
+            reference: `Immunization/${siId}`,
+          })),
+        },
+      ],
+      extension: [
+        {
+          url: `${settings.fhir.profileBaseUrl}/vp-immunization-recommendation-is-deactivated`,
+          valueBoolean: isDeactivated,
+        },
+        ...fulfillingImmunizationIds.map((fiId) => ({
+          url: `${settings.fhir.profileBaseUrl}/vp-fulfilling-immunization`,
+          valueReference: { reference: `Immunization/${fiId}` },
+        })),
+        {
+          url: `${settings.fhir.profileBaseUrl}/vp-supporting-population-recommendation`,
+          valueReference: { reference: `Basic/${populationRecommendationId}` },
+        },
+        {
+          url: `${settings.fhir.profileBaseUrl}/vp-recommended-vaccination-dose`,
+          valueReference: { reference: `Basic/${vaccinationDoseId}` },
+        },
+      ],
+    });
+  }
+
   toResource(): FHIRImmunizationRecommendation {
     return this._raw;
   }
@@ -76,63 +160,133 @@ export class ImmunizationRecommendationMapper
     return new Date(this._raw.date);
   }
 
-  get forecastStatus(): CodeableConcept {
-    const forecastStatusCoding = fhirpath.evaluate(
+  set date(date: Date) {
+    this._raw.date = dayjs(date).format('YYYY-MM-DD');
+  }
+
+  withDate(date: Date): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.date = date;
+    return newImmunizationRecommendation;
+  }
+
+  get _forecastStatusCoding(): FHIRCoding {
+    return fhirpath.evaluate(
       this._raw,
       `recommendation.forecastStatus.coding.where(system = 'http://terminology.hl7.org/CodeSystem/immunization-recommendation-status')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRCoding;
+  }
 
+  get forecastStatus(): CodeableConcept {
     return {
       coding: {
-        code: forecastStatusCoding.code!,
-        system: forecastStatusCoding.system!,
+        code: this._forecastStatusCoding.code!,
+        system: this._forecastStatusCoding.system!,
       },
     };
   }
 
-  get vaccineCode(): CodeableConcept {
-    const vaccineCodeCoding = fhirpath.evaluate(
+  set forecastStatus(forecastStatus: CodeableConcept) {
+    this._forecastStatusCoding.code = forecastStatus.coding.code;
+    this._forecastStatusCoding.system = forecastStatus.coding.system;
+  }
+
+  withForecastStatus(
+    forecastStatus: CodeableConcept
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.forecastStatus = forecastStatus;
+    return newImmunizationRecommendation;
+  }
+
+  get _vaccineCodeCoding(): FHIRCoding {
+    return fhirpath.evaluate(
       this._raw,
       `recommendation.vaccineCode.coding.where(system = 'http://fhir.de/CodeSystem/ifa/pzn')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRCoding;
+  }
 
+  get vaccineCode(): CodeableConcept {
     return {
       coding: {
-        code: vaccineCodeCoding.code!,
-        system: vaccineCodeCoding.system!,
+        code: this._vaccineCodeCoding.code!,
+        system: this._vaccineCodeCoding.system!,
       },
     };
   }
 
-  get targetDisease(): CodeableConcept {
-    const targetDiseaseCoding = fhirpath.evaluate(
+  set vaccineCode(vaccineCode: CodeableConcept) {
+    this._vaccineCodeCoding.code = vaccineCode.coding.code;
+    this._vaccineCodeCoding.system = vaccineCode.coding.system;
+  }
+
+  withVaccineCode(
+    vaccineCode: CodeableConcept
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.vaccineCode = vaccineCode;
+    return newImmunizationRecommendation;
+  }
+
+  get _targetDiseaseCoding(): FHIRCoding {
+    return fhirpath.evaluate(
       this._raw,
       `recommendation.targetDisease.coding.where(system = 'http://hl7.org/fhir/sid/icd-10')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRCoding;
+  }
 
+  get targetDisease(): CodeableConcept {
     return {
       coding: {
-        code: targetDiseaseCoding.code!,
-        system: targetDiseaseCoding.system!,
+        code: this._targetDiseaseCoding.code!,
+        system: this._targetDiseaseCoding.system!,
       },
     };
   }
 
-  get recommendedStartDate(): Date {
-    const earliestDateToGiveDateCriterion = fhirpath.evaluate(
+  set targetDisease(targetDisease: CodeableConcept) {
+    this._targetDiseaseCoding.code = targetDisease.coding.code;
+    this._targetDiseaseCoding.system = targetDisease.coding.system;
+  }
+
+  withTargetDisease(
+    targetDisease: CodeableConcept
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.targetDisease = targetDisease;
+    return newImmunizationRecommendation;
+  }
+
+  get _earliestDateToGiveDateCriterion(): FHIRImmunizationRecommendationRecommendationDateCriterion {
+    return fhirpath.evaluate(
       this._raw,
       `recommendation.dateCriterion.where(code.coding.where(system = 'http://loinc.org').code = '30981-5')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRImmunizationRecommendationRecommendationDateCriterion;
+  }
 
-    return new Date(earliestDateToGiveDateCriterion.value);
+  get recommendedStartDate(): Date {
+    return new Date(this._earliestDateToGiveDateCriterion.value);
+  }
+
+  set recommendedStartDate(recommendedStartDate: Date) {
+    this._earliestDateToGiveDateCriterion.value =
+      dayjs(recommendedStartDate).format('YYYY-MM-DD');
+  }
+
+  withRecommendedStartDate(
+    recommendedStartDate: Date
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.recommendedStartDate = recommendedStartDate;
+    return newImmunizationRecommendation;
   }
 
   get status(): ImmunizationRecommendationStatus {
@@ -151,43 +305,90 @@ export class ImmunizationRecommendationMapper
     return 'future';
   }
 
-  get isDeactivated(): boolean {
-    const isDeactivatedExtension = fhirpath.evaluate(
+  get _isDeactivatedExtension(): FHIRExtension {
+    return fhirpath.evaluate(
       this._raw,
       `extension.where(url = '${settings.fhir.profileBaseUrl}/vp-immunization-recommendation-is-deactivated')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRExtension;
+  }
 
-    return isDeactivatedExtension.valueBoolean!;
+  get isDeactivated(): boolean {
+    return this._isDeactivatedExtension.valueBoolean!;
+  }
+
+  set isDeactivated(isDeactivated: boolean) {
+    this._isDeactivatedExtension.valueBoolean = isDeactivated;
+  }
+
+  withIsDeactivated(isDeactivated: boolean): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.isDeactivated = isDeactivated;
+    return newImmunizationRecommendation;
   }
 
   get supportingImmunizationIds(): string[] {
-    const supportingImmunizationReferences = fhirpath.evaluate(
-      this._raw,
-      `recommendation.supportingImmunization`,
-      undefined,
-      fhirpath_r4_model
-    ) as FHIRReference[];
-
-    return supportingImmunizationReferences.map((reference) => {
-      const referenceParts = reference.reference!.split('/');
-      return referenceParts[referenceParts.length - 1];
-    });
+    return this._raw.recommendation[0].supportingImmunization!.map(
+      (reference) => {
+        const referenceParts = reference.reference!.split('/');
+        return referenceParts[referenceParts.length - 1];
+      }
+    );
   }
 
-  get fulfillingImmunizationIds(): string[] {
-    const fulfillingImmunizationExtensions = fhirpath.evaluate(
+  set supportingImmunizationIds(supportingImmunizationIds: string[]) {
+    this._raw.recommendation[0].supportingImmunization =
+      supportingImmunizationIds.map((siId) => ({
+        reference: `Immunization/${siId}`,
+      }));
+  }
+
+  withSupportingImmunizationIds(
+    supportingImmunizationIds: string[]
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.supportingImmunizationIds =
+      supportingImmunizationIds;
+    return newImmunizationRecommendation;
+  }
+
+  get _fulfillingImmunizationExtensions(): FHIRExtension[] {
+    return fhirpath.evaluate(
       this._raw,
       `extension.where(url = '${settings.fhir.profileBaseUrl}/vp-fulfilling-immunization')`,
       undefined,
       fhirpath_r4_model
     ) as FHIRExtension[];
+  }
 
-    return fulfillingImmunizationExtensions.map((extension) => {
+  get fulfillingImmunizationIds(): string[] {
+    return this._fulfillingImmunizationExtensions.map((extension) => {
       const referenceParts = extension.valueReference!.reference!.split('/');
       return referenceParts[referenceParts.length - 1];
     });
+  }
+
+  set fulfillingImmunizationIds(fulfillingImmunizationIds: string[]) {
+    this._raw.extension = this._raw
+      .extension!.filter(
+        (ex) => !this._fulfillingImmunizationExtensions.includes(ex)
+      )
+      .concat(
+        fulfillingImmunizationIds.map((fiId) => ({
+          url: `${settings.fhir.profileBaseUrl}/vp-fulfilling-immunization`,
+          valueReference: { reference: `Immunization/${fiId}` },
+        }))
+      );
+  }
+
+  withFulfillingImmunizationIds(
+    fulfillingImmunizationIds: string[]
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.fulfillingImmunizationIds =
+      fulfillingImmunizationIds;
+    return newImmunizationRecommendation;
   }
 
   get patientId(): string {
@@ -195,31 +396,72 @@ export class ImmunizationRecommendationMapper
     return referenceParts[referenceParts.length - 1];
   }
 
-  get populationRecommendationId(): string {
-    const supportingPopulationRecommendationExtension = fhirpath.evaluate(
+  set patientId(patientId: string) {
+    this._raw.patient.reference = `Patient/${patientId}`;
+  }
+
+  withPatientId(patientId: string): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.patientId = patientId;
+    return newImmunizationRecommendation;
+  }
+
+  get _supportingPopulationRecommendationExtension(): FHIRExtension {
+    return fhirpath.evaluate(
       this._raw,
       `extension.where(url = '${settings.fhir.profileBaseUrl}/vp-supporting-population-recommendation')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRExtension;
+  }
 
+  get populationRecommendationId(): string {
     const referenceParts =
-      supportingPopulationRecommendationExtension.valueReference!.reference!.split(
+      this._supportingPopulationRecommendationExtension.valueReference!.reference!.split(
         '/'
       );
     return referenceParts[referenceParts.length - 1];
   }
 
-  get vaccinationDoseId(): string {
-    const recommendedVaccinationDoseExtension = fhirpath.evaluate(
+  set populationRecommendationId(populationRecommendationId: string) {
+    this._supportingPopulationRecommendationExtension.valueReference!.reference = `Basic/${populationRecommendationId}`;
+  }
+
+  withPopulationRecommendationId(
+    populationRecommendationId: string
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.populationRecommendationId =
+      populationRecommendationId;
+    return newImmunizationRecommendation;
+  }
+
+  get _recommendedVaccinationDoseExtension(): FHIRExtension {
+    return fhirpath.evaluate(
       this._raw,
       `extension.where(url = '${settings.fhir.profileBaseUrl}/vp-recommended-vaccination-dose')`,
       undefined,
       fhirpath_r4_model
     )[0] as FHIRExtension;
+  }
 
+  get vaccinationDoseId(): string {
     const referenceParts =
-      recommendedVaccinationDoseExtension.valueReference!.reference!.split('/');
+      this._recommendedVaccinationDoseExtension.valueReference!.reference!.split(
+        '/'
+      );
     return referenceParts[referenceParts.length - 1];
+  }
+
+  set vaccinationDoseId(vaccinationDoseId: string) {
+    this._recommendedVaccinationDoseExtension.valueReference!.reference = `Basic/${vaccinationDoseId}`;
+  }
+
+  withVaccinationDoseId(
+    vaccinationDoseId: string
+  ): ImmunizationRecommendationMapper {
+    const newImmunizationRecommendation = cloneDeep(this);
+    newImmunizationRecommendation.vaccinationDoseId = vaccinationDoseId;
+    return newImmunizationRecommendation;
   }
 }
